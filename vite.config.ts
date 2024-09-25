42a3e2998249ce6c9a5af536d99c6a751183df1a
Ferrari Riccardo (DCET/BDO1-IT) <fer1cer@bosch.com>
2024-09-25 16:06:12 +0200

    t

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { ghPages } from 'vite-plugin-gh-pages';
 
+
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: '/', // sostituisci 'my-react-project' con il nome della tua repository
+  base: '/andreadominici', // sostituisci 'my-react-project' con il nome della tua repository
 });
